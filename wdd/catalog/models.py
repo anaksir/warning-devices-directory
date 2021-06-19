6c06a249193bde14bebb640f6314c50daae61c43
anaksir <anaksir@gmail.com>
2021-06-19 15:16:42 +0300

    add device model

--- wdd/catalog/models.py
+++ wdd/catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    '''
+    Модель устройства оповещения
+    '''
+    class DeviceType(models.TextChoices):
+        SIREN = 'SI', 'Siren'
+        SPEAKER = 'SP', 'Speaker'
+
+    name = models.CharField(
+        max_length=20,
+        unique=True,
+        help_text='Device name')
+
+    device_type = models.CharField(
+        max_length=2,
+        choices=DeviceType.choices,
+        help_text='Type of device'
+    )
+
+    latitude = models.DecimalField(
+        max_digits=8,
+        decimal_places=6)
+
+    longitude = models.DecimalField(
+        max_digits=9,
+        decimal_places=6)
+
+    address = models.CharField(
+        max_length=100,
+    )
+
+    radius = models.PositiveIntegerField(
+        help_text='Radius of coverage area (m)'
+    )
