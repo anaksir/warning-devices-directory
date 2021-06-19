4d5b26c5a47d617010144156eaef5f000eba866f
anaksir <anaksir@gmail.com>
2021-06-19 14:24:28 +0300

    start project

--- wdd/wdd/urls.py
+++ wdd/wdd/urls.py
@@ -1,4 +1,4 @@
-"""dwd URL Configuration
+"""wdd URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
