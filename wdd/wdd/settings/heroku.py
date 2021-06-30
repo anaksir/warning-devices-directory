543380298300d3f565f7a9543e37ce379e3072f4
anaksir <anaksir@gmail.com>
2021-06-30 10:39:30 +0300

    change settings

--- wdd/wdd/settings/heroku.py
+++ wdd/wdd/settings/heroku.py
@@ -1,7 +1,45 @@
 import django_heroku
+import os
 from .base import *
 
 
 django_heroku.settings(locals())
 
 DEBUG = False
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
+    'django.contrib.postgres',
+    'whitenoise.runserver_nostatic',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'catalog',
+    'api',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
